520a0fed7b22452c1f0e89197257908aa8f6f845
Benoit Fontaine <benoit.a.fontaine@axa.fr>
2017-03-23 09:58:52 +0100

    Update @angular/cli

--- vsts-fllower-cli/src/app/app.component.spec.ts
+++ vsts-fllower-cli/src/app/app.component.spec.ts
@@ -2,12 +2,18 @@ import { TestBed, async } from '@angular/core/testing';
 
 import { AppComponent } from './app.component';
 
+import { MaterialModule } from '@angular/material';
+import 'hammerjs';
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent
       ],
+      imports: [
+        MaterialModule
+      ]
     }).compileComponents();
   }));
 
@@ -20,7 +26,7 @@ describe('AppComponent', () => {
   it(`should have as title 'app works!'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app works!');
+    expect(app.title).toEqual('ALM Follower');
   }));
 
   it('should render title in a h1 tag', async(() => {
