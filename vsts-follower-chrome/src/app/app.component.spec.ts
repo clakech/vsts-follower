f79fae8d8f88108ea2734ce1d40858308f52b619
Benoit Fontaine <benoit.a.fontaine@axa.fr>
2017-03-23 13:58:51 +0100

    chore: update to Angular 4 and rename directory

--- vsts-follower-chrome/src/app/app.component.spec.ts
+++ vsts-follower-chrome/src/app/app.component.spec.ts
@@ -1,19 +1,19 @@
 import { TestBed, async } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { MaterialModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
-
-import { MaterialModule } from '@angular/material';
-import 'hammerjs';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule,
+        MaterialModule
+      ],
       declarations: [
         AppComponent
       ],
-      imports: [
-        MaterialModule
-      ]
     }).compileComponents();
   }));
 
@@ -23,16 +23,23 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   }));
 
-  it(`should have as title 'app works!'`, async(() => {
+  it(`should have as title 'alm works!'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('ALM Follower');
+    expect(app.title).toEqual('Alm Follower');
   }));
 
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('app works!');
+    expect(compiled.querySelector('h1').textContent).toContain('Alm Follower');
+  }));
+
+  it('should render loading spinner', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('md-spinner')).not.toBeNull();
   }));
 });
