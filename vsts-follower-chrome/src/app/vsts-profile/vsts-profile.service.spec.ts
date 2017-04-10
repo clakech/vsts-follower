bcde52175153a21e4187d5babc39de41d39f6603
Benoit Fontaine <benoit.a.fontaine@axa.fr>
2017-04-10 15:05:53 +0200

    feat(vsts-login): save connection data

--- vsts-follower-chrome/src/app/vsts-profile/vsts-profile.service.spec.ts
+++ vsts-follower-chrome/src/app/vsts-profile/vsts-profile.service.spec.ts
@@ -13,13 +13,51 @@ describe('VstsProfileService', () => {
     expect(service).toBeTruthy();
   }));
 
-  it('should not have null data', inject([VstsProfileService], (service: VstsProfileService) => {
-    expect(service.getVstsProfile()).not.toBeNull();
-    expect(service.getVstsProfile().login).not.toBeNull();
-    expect(service.getVstsProfile().token).not.toBeNull();
-    expect(service.getVstsProfile().url).not.toBeNull();
+  it('should return null if no data', inject([VstsProfileService], (service: VstsProfileService) => {
+    localStorage.removeItem(service.storageName);
+
+    expect(service.getVstsProfile()).toBeNull();
+  }));
+
+  it('should get data from localStorage', inject([VstsProfileService], (service: VstsProfileService) => {
+    const expected = {
+      url: 'https://axafrance.visualstudio.com',
+      login: 'user.name@axa.fr',
+      token: 's1234567'
+    };
+    localStorage.setItem(service.storageName, JSON.stringify(expected));
+
+    const currentDomain = service.getVstsProfile();
+
+    expect(currentDomain).not.toBeNull();
+    expect(currentDomain.login).toBe(expected.login);
+    expect(currentDomain.token).toBe(expected.token);
+    expect(currentDomain.url).toBe(expected.url);
   }));
 
 
+  it('should set data to localStorage', inject([VstsProfileService], (service: VstsProfileService) => {
+    const originalData = {
+      url: 'https://axafrance.visualstudio.com',
+      login: 'user.name@axa.fr',
+      token: 's1234567'
+    };
+    localStorage.setItem(service.storageName, JSON.stringify(originalData));
+
+    const expected = {
+      url: 'https://taichin.visualstudio.com',
+      login: 'user.name@outlook.com',
+      token: 'mdppowa'
+    };
+
+    service.setVstsProfile(expected);
+
+    const currentDomain = service.getVstsProfile();
+
+    expect(currentDomain).not.toBeNull();
+    expect(currentDomain.login).toBe(expected.login);
+    expect(currentDomain.token).toBe(expected.token);
+    expect(currentDomain.url).toBe(expected.url);
+  }));
 
 });
--- vsts-follower-chrome/src/app/vsts-profile/vsts-profile.service.ts
+++ vsts-follower-chrome/src/app/vsts-profile/vsts-profile.service.ts
@@ -3,14 +3,19 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class VstsProfileService {
 
-  private config = {
-    url: '',
-    login: '',
-    token: ''
-  };
+  public storageName = 'vstsProfile';
+
+  private config;
 
   public getVstsProfile() {
+    const dataStr = localStorage.getItem(this.storageName);
+    this.config =  JSON.parse(dataStr);
     return this.config;
+  }
+
+  public setVstsProfile(data: any) {
+    localStorage.setItem(this.storageName, JSON.stringify(data));
+    return (this.getVstsProfile() != null);
   }
 
   constructor() { }
