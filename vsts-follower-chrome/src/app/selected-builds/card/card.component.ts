07d9315dedc0fe0cab57b0689ebd6ea96b862bf7
Benoit Fontaine <benoit.a.fontaine@axa.fr>
2017-05-10 12:50:43 +0200

    feat(selection-mode): show selected builds and mock of Tests/Sonar

--- vsts-follower-chrome/src/app/selected-builds/card/card.component.ts
+++ vsts-follower-chrome/src/app/selected-builds/card/card.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { MainBuildsInfo, VstsBuild, VstsBuildDefinition, VstsProject } from '../../vsts/vsts-project';
+
+class QualityIndicator {
+  public name: string;
+  public value: string;
+  public color: string;
+}
 
 @Component({
   selector: 'follow-card',
@@ -7,9 +14,109 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardComponent implements OnInit {
 
+  @Input() build: MainBuildsInfo = new MainBuildsInfo(new VstsBuildDefinition());
+  public indicators: Array<QualityIndicator> = new Array<QualityIndicator>();
+
   constructor() { }
 
   ngOnInit() {
+    let curent = new QualityIndicator();
+    curent.color = "crimson";
+    curent.name = "Failed tests";
+    curent.value = "5";
+    this.indicators.push(curent);
+    curent = new QualityIndicator();
+    curent.color = "goldenrod";
+    curent.name = "Test Coverage";
+    curent.value = "79 %";
+    this.indicators.push(curent);
+    this.setSonarNullInformations();
+    curent = new QualityIndicator();
+    curent.color = "goldenrod";
+    curent.name = "Ignore tests";
+    curent.value = "5";
+    this.indicators.push(curent);
+  }
+
+  getDefaultColor() {
+    return "gainsboro";
+  }
+
+  getBuildColor(build: VstsBuild): string {
+    let color: string = "gainsboro";
+    if (build) {
+      switch (build.result) {
+        case "succeeded":
+          color = "limegreen";
+          break;
+        case "partiallySucceeded":
+          color = "goldenrod";
+          break;
+        case "failed":
+          color = "crimson";
+          break;
+        default:
+          color = "gainsboro";
+          break;
+      }
+    }
+    return color;
+  }
+
+  getColorByNote(note: string) {
+    let color: string;
+    switch (note) {
+      case "A":
+        color = "limegreen";
+        break;
+      case "B":
+        color = "lightgreen";
+        break;
+      case "C":
+        color = "khaki";
+        break;
+      case "D":
+        color = "goldenrod";
+        break;
+      case "E":
+        color = "crimson";
+        break;
+      default:
+        color = "gainsboro";
+        break;
+    }
+    return color;
+  }
+
+  setSonarNullInformations() {
+    this.indicators.push(
+      <QualityIndicator>{
+        name: "Reliability",
+        value: "-",
+        color: this.getColorByNote("-")
+      }
+    );
+    this.indicators.push(
+      <QualityIndicator>{
+        name: "Security",
+        value: "-",
+        color: this.getColorByNote("-")
+      }
+    );
+    this.indicators.push(
+      <QualityIndicator>{
+        name: "Maintainability",
+        value: "-",
+        color: this.getColorByNote("-")
+      }
+    );
+    this.indicators.push(
+      <QualityIndicator>{
+        name: "Coverage",
+        value: "-",
+        color: this.getColorByNote("F")
+      }
+    );
   }
 
 }
--- vsts-follower-chrome/src/app/selected-builds/selected-builds.module.ts
+++ vsts-follower-chrome/src/app/selected-builds/selected-builds.module.ts
@@ -1,15 +1,21 @@
+import 'hammerjs';
+
 import { CardComponent } from './card/card.component';
 import { CommonModule } from '@angular/common';
 import { ListComponent } from './list/list.component';
+import { MaterialModule } from '@angular/material';
 import { NgModule } from '@angular/core';
 import { SelectedBuildsService } from './selected-builds.service';
 import { SelectionToggleComponent } from './selection-toggle/selection-toggle.component';
+import { TileComponent } from './tile/tile.component';
+import { IndicatorTileComponent } from './indicator-tile/indicator-tile.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    MaterialModule
   ],
-  declarations: [ListComponent, CardComponent, SelectionToggleComponent],
+  declarations: [ListComponent, CardComponent, SelectionToggleComponent, TileComponent, IndicatorTileComponent],
   exports: [ListComponent],
   providers: [SelectedBuildsService]
 })
